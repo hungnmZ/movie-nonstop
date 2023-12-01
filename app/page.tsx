58ed2ff514b6b041b51738e7a79e7e10a3533273
hungnmZ <manhungg.99@gmail.com>
2023-12-01 12:21:07 +0700

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
-export default function Home() {
-  return <main>Hello world</main>;
-}
+import * as React from 'react';
+
+import CardSliderContainer from '@/components/CardSlider/CardSlider';
+import { getHomeTitles } from '@/data/title';
+
+const Home = async () => {
+  const data = await getHomeTitles();
+
+  return (
+    <main>
+      <div className='-mb-5'>
+        <CardSliderContainer
+          header='Trending Now'
+          titles={data.recommendedTitles?.nodes}
+        />
+      </div>
+
+      <div className='-mb-5'>
+        <CardSliderContainer
+          header='New Releases Movies'
+          titles={data.movieTitles?.nodes}
+        />
+      </div>
+
+      <div className='-mb-5'>
+        <CardSliderContainer
+          header='New Releases TV Shows'
+          titles={data.showTitles?.nodes}
+        />
+      </div>
+      <div style={{ height: '100vh' }}></div>
+    </main>
+  );
+};
+
+export default Home;
