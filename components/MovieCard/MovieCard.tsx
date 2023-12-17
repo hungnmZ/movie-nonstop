d9e04845cce79479b5b2adf2a4e033c077da93c7
hungnmZ <manhungg.99@gmail.com>
2023-12-18 00:29:19 +0700

    fix: refactor, add delay to transition to fix item from collapsing when hovered over

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -2,23 +2,25 @@
 
 import * as React from 'react';
 import dayjs from 'dayjs';
-import { motion } from 'framer-motion';
+import { motion, Transition } from 'framer-motion';
 import Image from 'next/image';
 
 import { BasicTitle } from '@/types/Title';
+import { debounce } from '@/utils';
 import { displayTitleTime } from '@/utils/time';
 
 import PlayCircle from '../Icons/PlayCircle';
 
-const SPRING = {
+const SPRING: Transition = {
   type: 'spring',
   stiffness: 200,
   damping: 30,
+  duration: 0.5,
 };
 
 interface MovieCardProps {
-  isFirst: boolean;
-  isLast: boolean;
+  isFirst?: boolean;
+  isLast?: boolean;
   data: BasicTitle;
 }
 
@@ -29,82 +31,84 @@ const MovieCard: React.FC<MovieCardProps> = ({
 }) => {
   const [isHover, setIsHover] = React.useState(false);
   const [zIndex, setZIndex] = React.useState(0);
-  const [isExitAnimation, setIsExitAnimation] = React.useState(false);
 
-  const getCardVariant = () => {
-    if (isHover && isFirst) return 'hover1stEl';
-    if (isHover && isLast) return 'hoverLastEl';
-    if (isHover) return 'hover';
-  };
+  const setZIndexDebounce = React.useMemo(() => debounce(setZIndex, 500), []);
 
   return (
     <div
-      onMouseLeave={() => setIsHover(false)}
-      onMouseOver={() => setIsHover(true)}
-      className='cardWrapper relative h-full w-full cursor-pointer overflow-visible'
-      style={{
-        zIndex: isHover ? 12 : zIndex,
+      className='cardWrapper relative h-full w-full'
+      onMouseEnter={() => {
+        setIsHover(true);
+        setZIndexDebounce(20);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+        setZIndexDebounce(0);
       }}
     >
       <motion.div
-        className='cardContent absolute inset-0 flex flex-col overflow-hidden rounded-lg shadow-md shadow-foreground/20 dark:shadow-background/20'
-        initial={false}
+        className='cardContent absolute flex cursor-pointer flex-col overflow-hidden rounded-lg shadow-md shadow-foreground/20 dark:shadow-background/20'
+        style={{ zIndex }}
         transition={SPRING}
-        animate={getCardVariant()}
+        animate={
+          (isHover && isFirst && ['hover', 'hover1stEl']) ||
+          (isHover && isLast && ['hover', 'hoverLastEl']) ||
+          (isHover && 'hover') ||
+          'default'
+        }
+        initial={false}
         variants={{
-          hover: { top: '-20%', bottom: '-20%', left: '-20%', right: '-20%' },
-          hover1stEl: { top: '-20%', bottom: '-20%', left: 0, right: '-40%' },
-          hoverLastEl: { top: '-20%', bottom: '-20%', left: '-40%', right: 0 },
-        }}
-        onAnimationStart={() => setZIndex(11)}
-        onAnimationComplete={() => {
-          !isHover && isExitAnimation && setZIndex(0);
-          setIsExitAnimation(!isExitAnimation);
+          default: { inset: 0 },
+          hover: {
+            inset: '-20%',
+            left: '-20%',
+            right: '-20%',
+            transition: { delay: 0.5 },
+          },
+          hover1stEl: { left: 0, right: '-40%', transition: { delay: 0.5 } },
+          hoverLastEl: { left: '-40%', right: 0, transition: { delay: 0.5 } },
         }}
       >
         <Image
-          className='imageCard absolute inset-0 h-full w-full '
+          className='imageCard h absolute h-full w-full'
           width={320}
           height={180}
           src={data.tmdbBackdrop}
-          alt='Loki'
+          alt={data.nameEn}
         />
         <motion.div
           className='filterColor absolute inset-0 bg-black'
-          initial={false}
           transition={SPRING}
-          animate={{
-            opacity: isHover ? 0.5 : 0,
+          variants={{
+            default: { opacity: 0 },
+            hover: { opacity: 0.5 },
           }}
         />
         <motion.div
           className='playIcon relative flex w-full justify-end'
-          initial={false}
           transition={SPRING}
-          animate={{
-            opacity: isHover ? 1 : 0,
+          variants={{
+            default: { opacity: 0 },
+            hover: { opacity: 1 },
           }}
         >
           <div className='mr-2 mt-2 aspect-square w-1/6 transition-transform hover:scale-110'>
             <PlayCircle className='h-full w-full' width={60} height={60} />
           </div>
         </motion.div>
-
-        <div className='mb-2 ml-3 flex flex-1 flex-col justify-end'>
+        <div className='mx-3 mb-2 flex flex-1 flex-col justify-end'>
           <div
             style={{ textShadow: '#000 0px 0px 6px' }}
-            className='cardTitle relative whitespace-break-spaces text-lg font-semibold text-white xl:text-xl'
+            className='cardTitle relative whitespace-normal text-lg font-semibold text-white xl:text-xl'
           >
             {data.nameEn}
           </div>
           <motion.div
             className='cardPreview relative overflow-hidden text-white'
-            initial={false}
             transition={SPRING}
-            animate={{
-              width: isHover ? '100%' : 0,
-              height: isHover ? 'auto' : 0,
-              opacity: isHover ? 1 : 0,
+            variants={{
+              default: { opacity: 0, y: 20, height: 0 },
+              hover: { opacity: 1, y: 0, height: 'auto' },
             }}
           >
             <div className='cardInfo text-sm text-gray-400'>
@@ -115,10 +119,10 @@ const MovieCard: React.FC<MovieCardProps> = ({
               )}
               {data.imdbRating && <span>- IMDb {data.imdbRating} </span>}
             </div>
-            <div className='evidenceList flex'>
+            <div className='evidenceList overflow-hidden overflow-ellipsis'>
               {data.genres.length &&
                 data.genres.map(({ nameEn }) => (
-                  <div className='evidenceItem content-dot' key={nameEn}>
+                  <div className='evidenceItem content-dot inline-block' key={nameEn}>
                     <span className='evidenceText whitespace-nowrap text-sm'>
                       {nameEn}
                     </span>
--- utils/index.ts
+++ utils/index.ts
@@ -9,3 +9,13 @@ export const range = (start: number, end: number, step = 1) => {
   }
   return output;
 };
+
+export const debounce = (func: Function, delay: number) => {
+  let debounceTimer: ReturnType<typeof setTimeout>;
+
+  return function (this: any, ...args: any[]) {
+    const context = this;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
