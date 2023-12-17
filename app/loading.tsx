40d0441be8b8fa97a1a06e73a3d0b5bfb5607072
hungnmZ <manhungg.99@gmail.com>
2023-12-18 00:19:06 +0700

    feat: add home loading page

---
+++ app/loading.tsx
@@ -0,0 +1,46 @@
+import MovieCardSkeleton from '@/components/MovieCardSkeleton';
+import { range } from '@/utils';
+
+export default function Loading() {
+  return (
+    <main>
+      <div className='-mb-5 overflow-visible whitespace-nowrap p-5 md:p-10'>
+        <div className='mb-2 text-xl font-extrabold xl:text-2xl'>Trending Now</div>
+        {range(0, 12).map((index) => (
+          <div
+            key={index}
+            className='inline-block aspect-video w-1/3 px-1 align-top first:pl-0 last:pr-0 lg:w-1/4 xl:w-1/6'
+          >
+            <MovieCardSkeleton />
+          </div>
+        ))}
+      </div>
+
+      <div className='-mb-5 overflow-visible whitespace-nowrap p-5 md:p-10'>
+        <div className='mb-2 text-xl font-extrabold xl:text-2xl'>New Releases Movies</div>
+        {range(0, 12).map((index) => (
+          <div
+            key={index}
+            className='inline-block aspect-video w-1/3 px-1 align-top first:pl-0 last:pr-0 lg:w-1/4 xl:w-1/6'
+          >
+            <MovieCardSkeleton />
+          </div>
+        ))}
+      </div>
+
+      <div className='-mb-5 overflow-visible whitespace-nowrap p-5 md:p-10'>
+        <div className='mb-2 text-xl font-extrabold xl:text-2xl'>
+          New Releases TV Shows
+        </div>
+        {range(0, 12).map((index) => (
+          <div
+            key={index}
+            className='inline-block aspect-video w-1/3 px-1 align-top first:pl-0 last:pr-0 lg:w-1/4 xl:w-1/6'
+          >
+            <MovieCardSkeleton />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
