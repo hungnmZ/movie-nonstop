506343a2b9b07b0f9dc5f429da8085935686a801
hungnmZ <manhungg.99@gmail.com>
2023-12-18 01:57:37 +0700

    feat: add new 404 and error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-const GlobalError = ({
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) => (
+const GlobalError = () => (
   <html>
     <body>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
+      <div className='flex h-screen'>
+        <div className='m-auto flex'>
+          <h1 className='leading-12 mx-5 my-0 inline-block border-r border-gray-800 pr-6 align-top text-4xl font-bold dark:border-gray-200'>
+            Oops!
+          </h1>
+          <div className='flex items-center'>
+            <h2 className='leading-12 m-0 font-light'>Something went wrong.</h2>
+          </div>
+        </div>
+      </div>
     </body>
   </html>
 );
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,13 @@
-import Link from 'next/link';
-
 const NotFound = () => (
-  <div>
-    <h2>Not Found</h2>
-    <p>Could not find requested resource</p>
-    <Link href='/'>Return Home</Link>
+  <div className='fixed inset-0 top-14 flex'>
+    <div className='m-auto flex'>
+      <h1 className='leading-12 mx-5 my-0 inline-block border-r border-gray-800 pr-6 align-top text-4xl font-bold dark:border-gray-200'>
+        404
+      </h1>
+      <div className='flex items-center'>
+        <h2 className='leading-12 m-0 font-light'>This page could not be found.</h2>
+      </div>
+    </div>
   </div>
 );
 
